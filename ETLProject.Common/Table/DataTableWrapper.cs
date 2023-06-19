2dfa5c3cc9804a9f4baccff20b030882e664bb51
alirezaeiji <alirezaeiji151479@gmail.com>
2023-06-19 17:22:48 +0430

    Add DataTableWapper and its dependent classes

---
+++ ETLProject.Common/Table/DataTableWrapper.cs
@@ -0,0 +1,13 @@
+﻿using ETLProject.Common.Database;
+using System.Data;
+
+namespace ETLProject.Common.Table
+{
+    public class DataTableWrapper
+    {
+        public DataSourceType DataSourceType { get; set; }
+        public TableType TableType { get; set; }
+        public DataTable DataTable { get; set; }
+
+    }
+}
