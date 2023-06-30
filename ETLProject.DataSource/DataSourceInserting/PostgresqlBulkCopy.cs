aa155ce7a86e38d355bb28f90a64c75310950cde
alirezaeiji <alirezaeiji151479@gmail.com>
2023-06-30 21:50:49 +0330

    Add Postgresql Bulk Insert

--- ETLProject.DataSource/DataSourceInserting/PostgresqlBulkCopy.cs
+++ ETLProject.DataSource/DataSourceInserting/PostgresqlBulkCopy.cs
@@ -1,6 +1,7 @@
 ﻿using ETLProject.Common.Database;
 using ETLProject.Common.Table;
 using ETLProject.DataSource.Abstractions;
+using Npgsql;
 using System.Data;
 
 namespace ETLProject.DataSource.DataSourceInserting
@@ -9,9 +10,40 @@ namespace ETLProject.DataSource.DataSourceInserting
     {
         public DataSourceType DataSourceType => DataSourceType.Postgresql;
 
-        public Task InsertBulk(DataTable dataTable, ETLTable etlTable)
+        public async Task InsertBulk(DataTable dataTable, ETLTable etlTable)
         {
-            throw new NotImplementedException();
+            //TODO : refactor query making
+            var columnSet = string.Format($"({string.Join(",",etlTable.Columns.Select(x => x.Name))})");
+            var importQueryString = $"COPY {etlTable.TableName} {columnSet} FROM STDIN (FORMAT BINARY)";
+            using var writer = (etlTable.DbConnection as NpgsqlConnection).BeginBinaryImport(importQueryString);
+            foreach (DataRow row in dataTable.Rows)
+            {
+                await writer.StartRowAsync();
+                for(var i = 0;i< etlTable.Columns.Count; i++)
+                {
+                    switch (etlTable.Columns[i].ETLColumnType.Type)
+                    {
+                        case ColumnType.StringType:
+                            await writer.WriteAsync(row[i],NpgsqlTypes.NpgsqlDbType.Varchar).ConfigureAwait(false);
+                            break;
+                        case ColumnType.Int32Type:
+                            await writer.WriteAsync(row[i],NpgsqlTypes.NpgsqlDbType.Integer).ConfigureAwait(false);
+                            break;
+                        case ColumnType.DoubleType:
+                            await writer.WriteAsync(row[i],NpgsqlTypes.NpgsqlDbType.Double).ConfigureAwait(false);
+                            break;
+                        case ColumnType.LongType:
+                            await writer.WriteAsync(row[i],NpgsqlTypes.NpgsqlDbType.Bigint).ConfigureAwait(false);
+                            break;
+                        case ColumnType.BooleanType:
+                            await writer.WriteAsync(row[i],NpgsqlTypes.NpgsqlDbType.Boolean).ConfigureAwait(false);
+                            break;
+                        default:
+                            throw new NotSupportedException();
+                    }
+                }
+            }
+            await writer.CompleteAsync();
         }
     }
 }
