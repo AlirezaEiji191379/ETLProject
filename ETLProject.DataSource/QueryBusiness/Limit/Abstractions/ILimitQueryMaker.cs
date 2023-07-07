7e6ceb273c8063f7b8b756c74bcdd4509022665a
alirezaeiji <alirezaeiji151479@gmail.com>
2023-07-07 12:10:08 +0330

    Add Limit for query limit contract and new project for contracts

---
+++ ETLProject.DataSource/QueryBusiness/Limit/Abstractions/ILimitQueryMaker.cs
@@ -0,0 +1,9 @@
+﻿using ETLProject.Common.Table;
+using ETLProject.Contract.Limit;
+
+namespace ETLProject.DataSource.QueryBusiness.Limit.Abstractions;
+
+public interface ILimitQueryMaker
+{
+    void AddLimitQuery(ETLTable etlTable,LimitContract limitContract);
+}
---
+++ ETLProject.DataSource/QueryBusiness/Limit/LimitQueryMaker.cs
@@ -0,0 +1,14 @@
+﻿using ETLProject.Common.Table;
+using ETLProject.Contract.Limit;
+using ETLProject.DataSource.QueryBusiness.Limit.Abstractions;
+
+namespace ETLProject.DataSource.QueryBusiness.Limit;
+
+public class LimitQueryMaker : ILimitQueryMaker
+{
+    public void AddLimitQuery(ETLTable etlTable, LimitContract limitContract)
+    {
+        var query = etlTable.Query;
+        query.Limit(limitContract.Top);
+    }
+}
