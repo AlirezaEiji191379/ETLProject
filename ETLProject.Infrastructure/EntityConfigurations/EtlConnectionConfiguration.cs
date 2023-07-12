ff909e02a9a3d9a955cda561102caf354bbb50a3
alirezaeiji <alirezaeiji151479@gmail.com>
2023-07-12 14:15:04 +0330

    Add Migration for etlconnection

---
+++ ETLProject.Infrastructure/EntityConfigurations/EtlConnectionConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using ETLProject.Common.Database;
+using ETLProject.Infrastructure.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ETLProject.Infrastructure.EntityConfigurations;
+
+public class EtlConnectionConfiguration : IEntityTypeConfiguration<EtlConnection>
+{
+    public void Configure(EntityTypeBuilder<EtlConnection> builder)
+    {
+        builder.HasKey(x => x.Id);
+        builder.Property(x => x.DataSourceType)
+            .HasConversion(t => t.ToString(),
+                t => (DataSourceType)Enum.Parse(typeof(DataSourceType), t));
+    }
+}
---
+++ ETLProject.Infrastructure/Migrations/20230712102806_init_etl_connection_table.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ETLProject.Infrastructure.Migrations
+{
+    public partial class init_etl_connection_table : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "EtlConnections",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Host = table.Column<string>(type: "text", nullable: false),
+                    Port = table.Column<string>(type: "text", nullable: false),
+                    Username = table.Column<string>(type: "text", nullable: false),
+                    Password = table.Column<string>(type: "text", nullable: false),
+                    DatabaseName = table.Column<string>(type: "text", nullable: false),
+                    ConnectionName = table.Column<string>(type: "text", nullable: false),
+                    Schema = table.Column<string>(type: "text", nullable: false),
+                    DataSourceType = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EtlConnections", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EtlConnections");
+        }
+    }
+}
