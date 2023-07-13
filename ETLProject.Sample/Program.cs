ff28e2f6a11799946ed6b8b4c05e445afd2260ca
Alireza Eiji <alirezaeiji151379@gmail.com>
2023-07-13 17:38:49 +0330

    Db Reader Ends!

--- ETLProject.Sample/Program.cs
+++ ETLProject.Sample/Program.cs
@@ -1,32 +1,4 @@
 ﻿
-using ETLProject.Common.Common.DIManager;
-using ETLProject.Common.Database;
-using ETLProject.Contract.DbConnectionContracts;
-using ETLProject.DataSource.Abstractions;
-using ETLProject.DataSource.Common.DIManager;
-using Microsoft.Extensions.DependencyInjection;
-
-var serviceCollection = new ServiceCollection();
-
-serviceCollection.AddCommonServices();
-serviceCollection.AddDataSourceQueryServices();
-
-var provider = serviceCollection.BuildServiceProvider();
-
-var dbConnectionMetaDataProvider = provider.GetRequiredService<IDbConnectionMetaDataBusinessProvider>();
-
-
-var sqlServerMetaDataBusiness = dbConnectionMetaDataProvider.GetMetaDataBusiness(DataSourceType.MySql);
-
-var result = await sqlServerMetaDataBusiness.GetTableColumns(new ConnectionDto()
-{
-    DataSourceType = DataSourceType.MySql,
-    Host = "localhost",
-    Password = "92?VH2WMrx",
-    Port = "3306",
-    Username = "alirezaeiji"
-},"kheft","user");
-
 Console.WriteLine();
 
 
