5fc2fe2818c22a1a630074aa0e774958be031dc4
alirezaeiji <alirezaeiji151479@gmail.com>
2023-07-31 01:26:43 +0330

    add pipeline started

---
+++ ETLProject.Pipeline/Abstractions/Plugin.cs
@@ -0,0 +1,22 @@
+﻿using ETLProject.Common.PipeLine.Abstractions;
+using ETLProject.Common.PipeLine.Enums;
+using ETLProject.Common.Table;
+
+namespace ETLProject.Pipeline.Abstractions;
+
+public abstract class Plugin
+{
+    Guid PluginId { get; init; }
+    PluginType PluginType { get; }
+    string PluginTitle { get; init; }
+    IPluginConfig PluginConfig { get; init; }
+    int InputPlugins { get; }
+    int? OutputPlugins { get; }
+    public abstract void AddInputPlugin(Guid pluginId);
+    public abstract void AddOutputPlugin(Guid pluginId);
+
+    public virtual void AddResultETLTableToNextPlugins(Guid currentPluginId, ETLTable etlTable)
+    {
+        
+    }
+}
